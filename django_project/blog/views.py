bf0d21628c29bd3109e5a313670fbac8ef56cdea
Ryan Febriansyah <37971350+sodrooome@users.noreply.github.com>
2018-12-15 12:58:34 +0700

    added some function

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView
 # Create your views here.
-
+"""dummy data
 posts = [
     {
         'author' : 'Ryan',
@@ -17,6 +18,7 @@ posts = [
         'date_posted' : '7 Desember 2018'
     }
 ]
+"""
 
 def home(request):
     context = {
@@ -26,3 +28,16 @@ def home(request):
 
 def about(request):
     return render(request, 'blog/about.html', {'title' : 'About'})
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ('title', 'content')
